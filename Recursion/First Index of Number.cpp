e90623740f340240d132c46a73b27797c9a31cd9
Ronak Sinha <ronaksinha1998@gmail.com>
2022-06-25 22:53:43 +0530

    Recursion

--- Recursion/First Index of Number.cpp
+++ Recursion/First Index of Number.cpp
@@ -1,5 +1,4 @@
 // First Index of Number
-// Send Feedback
 // Given an array of length N and an integer x, you need to find and return the first index of integer x present in the array. Return -1 if it is not present in the array.
 // First index means, the index of first occurrence of x in the input array.
 // Do this recursively. Indexing in the array starts from 0.
